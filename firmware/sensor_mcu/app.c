f4501bf22bfbf866772bd831755e5ae8cd8b76ed
dieramoro <ramirezdiego27@gmail.com>
2026-04-17 17:17:12 -0400

    debug LED test - working

--- firmware/sensor_mcu/app.c
+++ firmware/sensor_mcu/app.c
@@ -15,11 +15,75 @@
  *
  ******************************************************************************/
 
+#include <stdint.h>
+#include "em_cmu.h"
+#include "em_gpio.h"
+
+enum {
+  test_pin_bit0 = 7,
+  test_pin_bit1 = 8,
+  test_pin_bit2 = 9,
+};
+
+static volatile uint32_t systick_ms;
+static uint32_t last_update_ms;
+static uint8_t test_counter;
+
+static void test_output_binary(uint8_t value)
+{
+  if (value & 0x1U) {
+    GPIO_PinOutSet(gpioPortC, test_pin_bit0);
+  } else {
+    GPIO_PinOutClear(gpioPortC, test_pin_bit0);
+  }
+
+  if (value & 0x2U) {
+    GPIO_PinOutSet(gpioPortC, test_pin_bit1);
+  } else {
+    GPIO_PinOutClear(gpioPortC, test_pin_bit1);
+  }
+
+  if (value & 0x4U) {
+    GPIO_PinOutSet(gpioPortC, test_pin_bit2);
+  } else {
+    GPIO_PinOutClear(gpioPortC, test_pin_bit2);
+  }
+}
+
+static void test_binary_counter_init(void)
+{
+  GPIO_PinModeSet(gpioPortC, test_pin_bit0, gpioModePushPull, 0);
+  GPIO_PinModeSet(gpioPortC, test_pin_bit1, gpioModePushPull, 0);
+  GPIO_PinModeSet(gpioPortC, test_pin_bit2, gpioModePushPull, 0);
+
+  test_output_binary(test_counter);
+
+  // Generate a 1 ms timebase so the super loop can update once per second.
+  SysTick_Config(CMU_ClockFreqGet(cmuClock_SYSCLK) / 1000U);
+}
+
+static void test_binary_counter_process_action(void)
+{
+  if ((systick_ms - last_update_ms) < 1000U) {
+    return;
+  }
+
+  last_update_ms = systick_ms;
+  test_counter = (test_counter + 1U) & 0x7U;
+  test_output_binary(test_counter);
+}
+
+void SysTick_Handler(void)
+{
+  systick_ms++;
+}
+
 /***************************************************************************//**
  * Initialize application.
  ******************************************************************************/
 void app_init(void)
 {
+  test_binary_counter_init();
 }
 
 /***************************************************************************//**
@@ -27,4 +91,5 @@ void app_init(void)
  ******************************************************************************/
 void app_process_action(void)
 {
+  test_binary_counter_process_action();
 }
