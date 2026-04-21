31fa5c59963123b2d3525d56426a725ac3239728
dieramoro <ramirezdiego27@gmail.com>
2026-04-21 17:16:53 -0400

    UART update

--- firmware/sensor_mcu/app.c
+++ firmware/sensor_mcu/app.c
@@ -20,7 +20,42 @@
 #include <stdio.h>
 
 #include "debug_console.h"
+#include "em_cmu.h"
+#include "em_eusart.h"
+#include "em_gpio.h"
 #include "sensor_bus.h"
+
+#define APP_UART_PERIPHERAL            EUSART1
+#define APP_UART_BAUDRATE              115200U
+#define APP_UART_RX_PORT               gpioPortC
+#define APP_UART_RX_PIN                0U
+#define APP_UART_TX_PORT               gpioPortC
+#define APP_UART_TX_PIN                1U
+
+static void app_uart_init(void)
+{
+  EUSART_UartInit_TypeDef init = EUSART_UART_INIT_DEFAULT_HF;
+
+  CMU_ClockEnable(cmuClock_GPIO, true);
+  CMU_ClockEnable(cmuClock_EUSART1, true);
+
+  GPIO_PinModeSet(APP_UART_TX_PORT, APP_UART_TX_PIN, gpioModePushPull, 1);
+  GPIO_PinModeSet(APP_UART_RX_PORT, APP_UART_RX_PIN, gpioModeInputPull, 1);
+
+  GPIO->EUSARTROUTE[EUSART_NUM(APP_UART_PERIPHERAL)].TXROUTE =
+    (APP_UART_TX_PORT << _GPIO_EUSART_TXROUTE_PORT_SHIFT)
+    | (APP_UART_TX_PIN << _GPIO_EUSART_TXROUTE_PIN_SHIFT);
+  GPIO->EUSARTROUTE[EUSART_NUM(APP_UART_PERIPHERAL)].RXROUTE =
+    (APP_UART_RX_PORT << _GPIO_EUSART_RXROUTE_PORT_SHIFT)
+    | (APP_UART_RX_PIN << _GPIO_EUSART_RXROUTE_PIN_SHIFT);
+  GPIO->EUSARTROUTE[EUSART_NUM(APP_UART_PERIPHERAL)].ROUTEEN =
+    GPIO_EUSART_ROUTEEN_TXPEN | GPIO_EUSART_ROUTEEN_RXPEN;
+
+  init.enable = eusartDisable;
+  init.baudrate = APP_UART_BAUDRATE;
+  EUSART_UartInitHf(APP_UART_PERIPHERAL, &init);
+  EUSART_Enable(APP_UART_PERIPHERAL, eusartEnable);
+}
 
 void app_init_early(void)
 {
@@ -33,6 +68,8 @@ void app_init_early(void)
 void app_init(void)
 {
   sensor_bus_state_t sensors = sensor_bus_init();
+
+  app_uart_init();
 
   printf("SENSORS_READY bme68x=%u sps30=%u\r\n",
          sensors.bme68x_present,
